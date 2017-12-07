102ff60a2e6c5ce5ecefb3d805a29b087b57370f
totoki <totoki@am.ics.keio.ac.jp>
2017-12-07 14:56:40 +0900

    added variables of chip information

--- 3D_API/lcf.py
+++ 3D_API/lcf.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 import os
-
 import sys
+
+thickness_of_chip = 0.00015 ## (meter)  default:150um
+vertical_distance_of_chip = '2.0e-5' ## (meter) default:20um
+
+
 
 if (len(sys.argv) != 2):
         sys.stderr.write("Usage: " + sys.argv[0] + " <input file (.dat)>\n")
@@ -30,10 +34,10 @@ layer_num = layer[len(layer)-1]
 
 for i in xrange(0, layer_num):
 	os.system("echo  \"" +str(2*i)+"\"  >> test.lcf")
-	os.system("echo  \"Y\nY\n1.75e6\n0.01\n0.00015\"  >> test.lcf")  # default chip config data 
+	os.system("echo  \"Y\nY\n1.75e6\n0.01\n"+str(thickness_of_chip)+"\"  >> test.lcf")  # default chip config data 
 	os.system("echo  \"test" +str(i+1)+".flp\n\"  >> test.lcf")
 	os.system("echo  \"" +str(2*i+1)+"\"  >> test.lcf")
-	os.system("echo  \"Y\nN\n4e6\n0.25\n2.0e-05\"  >> test.lcf")  # default tim config data 
+	os.system("echo  \"Y\nN\n4e6\n0.25\n"+str(vertical_distance_of_chip)+"\"  >> test.lcf")  # default tim config data 
 	os.system("echo  \"testTIM.flp\n\"  >> test.lcf")
 
 	
