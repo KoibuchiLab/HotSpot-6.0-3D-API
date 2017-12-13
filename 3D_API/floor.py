4ac15843d486ebf18110a99c4cfd048e7606705b
mkoibuchi <koibuchi@nii.ac.jp>
2017-12-13 11:23:05 +0900

    add base3 chip for micro2017

--- 3D_API/floor.py
+++ 3D_API/floor.py
@@ -88,6 +88,10 @@ for line in data_lines:
 		chip_xlen += [float(base2_x)]
 		chip_ylen += [float(base2_y)]
 		chip_name += ['base2']
+	elif 'base3' in str(data[0]):
+		chip_xlen += [float(base2_x)]
+		chip_ylen += [float(base2_y)]
+		chip_name += ['base2']
 	elif 'spreader' in str(data[0]):
 		chip_xlen += [float(spreader_x)]
 		chip_ylen += [float(spreader_y)]
