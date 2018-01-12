2ec8542e0768faf0742574ad0feb55952a22e278
lambert leong <lamberttleong@gmail.com>
2018-01-12 13:40:13 -1000

    minor change

--- 3D_API/nulldata_file.py
+++ 3D_API/nulldata_file.py
@@ -9,7 +9,7 @@ import operator
 class nulldata_file(object):
 
 	def __init__(self, null_data):
-		
+
 		self.__null_data = []
 		self.__null_layer = []
 		self.__name = []
@@ -18,33 +18,33 @@ class nulldata_file(object):
 		self.__null_x_len = []
 		self.__null_y_len = []
 		self.process_file(null_data)
-		
+
 	def get_null_data(self):
 		return self.__null_data
-		
+
 	def get_null_layer(self):
 		return self.__null_layer
-		
+
 	def get_name(self):
 		return self.__name
-		
+
 	def get_null_x(self):
 		return self.__null_x
-		
+
 	def get_null_y(self):
 		return self.__null_y
-		
+
 	def get_null_x_len(self):
 		return self.__null_x_len
-		
+
 	def get_null_y_len(self):
 		return self.__null_y_len
-		
+
 	def process_file(self, null_data):
 		read = open(null_data)
 		null_object = read.readlines()
 		read.close
-		
+
 		for line in null_object:
 			data2 = line[:-1].split(' ')
 			#print "data2 is " + str(data2)
@@ -55,4 +55,3 @@ class nulldata_file(object):
 			self.__null_y += [float(data2[3])]
 			self.__null_x_len += [float(data2[4])]
 			self.__null_y_len += [float(data2[5])]
-			
